2871bef7cdc270ecdd2d34e133ebb14b42c652f0
Sreeja-Pasunuti-dev <psreeja875@gmail.com>
2025-05-03 13:51:03 +0530

    Update events.js

--- js/events.js
+++ js/events.js
@@ -141,7 +141,7 @@ function loadClubEvents() {
                     
                     <div class="event-actions">
                         <a href="${event.registrationLink}" target="_blank" class="btn btn-primary">
-                            ${event.status === 'Registration Open' ? 'Register Now' :  'Registration Closed' }
+                            ${event.status === 'Registrations Open' ? 'Register Now' :  'Registration Closed' }
                         </a>
                     </div>
                 </div>
